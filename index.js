b84441704dd2025551f2a9f3d035d6d44aaf9104
Vitosnatios <vitosdeveloper@gmail.com>
2022-09-20 22:55:26 -0300

    made a lot of functions async, some try/catch, simplified some endpoints...

--- index.js
+++ index.js
@@ -39,152 +39,145 @@ const postCountSchema = {
 const Post = mongoose.model('post', postSchema);
 const PostCount = mongoose.model('postCount', postCountSchema);
 
+const sendStatus = (status) => {
+  return res.json({
+    status: status,
+  });
+};
+
 //BUMP!
-function bump(bumpId) {
-  Post.find({ randomIdGeneratedByMe: bumpId }, (err, result) => {
-    //BUMP! id deve ir no query aqui
+const bump = async (bumpId) => {
+  try {
+    const threadToBump = await Post.find({ randomIdGeneratedByMe: bumpId });
+    const oldReply = [...threadToBump][0];
+    await Post.create({
+      board: oldReply.board,
+      email: oldReply.email,
+      assunto: oldReply.assunto,
+      postContent: oldReply.postContent,
+      inserir: oldReply.inserir,
+      password: oldReply.password,
+      reply: oldReply.reply,
+      op: oldReply.op,
+      randomIdGeneratedByMe: oldReply.randomIdGeneratedByMe,
+      imgShow: oldReply.imgShow,
+      catUrl: oldReply.catUrl,
+      catWidth: oldReply.catWidth,
+      catHeight: oldReply.catHeight,
+      postDay: oldReply.postDay,
+    });
+    await Post.deleteMany({ _id: oldReply._id });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    const oldReply = [...result][0]; //Bota o post numa variavel
-
-    function copiarPost() {
-      Post.create({
-        //recria ela inteira pra ir pro topo
-        board: oldReply.board,
-        email: oldReply.email,
-        assunto: oldReply.assunto,
-        postContent: oldReply.postContent,
-        inserir: oldReply.inserir,
-        password: oldReply.password,
-        reply: oldReply.reply,
-        op: oldReply.op,
-        randomIdGeneratedByMe: oldReply.randomIdGeneratedByMe,
-        imgShow: oldReply.imgShow,
-        catUrl: oldReply.catUrl,
-        catWidth: oldReply.catWidth,
-        catHeight: oldReply.catHeight,
-        postDay: oldReply.postDay,
-      });
-    }
-
-    function deletarPostOriginal() {
-      Post.deleteMany({ _id: oldReply._id }, (err) => {
-        err ? console.log(err) : null;
-      }); //deleta o antigo! ha ha
-    }
-
-    copiarPost();
-    deletarPostOriginal();
-  }); //só que aí tem que setar as replys pra acharem o post por algo que nao seja o _id :/ mas ótimo arrumei
-}
-
-app.get('/api', (req, res) => {
-  Post.find({}, (err, postsFound) => {
-    res.json(postsFound); //envia db do server pro client, pra pegar dando useEffect no /api
-  })
-    .select('-password')
-    .sort({ $natural: -1 }); //.sort({$natural:-1}) faz ele entregar a DB de trás pra frente
+app.get('/api', async (req, res) => {
+  try {
+    const posts = await Post.find().select('-password');
+    return res.json(posts);
+  } catch (err) {
+    return sendStatus(err);
+  }
 });
 
-app.get('/reply', (req, res) => {
-  Post.find({}, (err, postsFound) => {
-    res.json(postsFound);
-  }).select('-password');
-});
-
-app.post('/newpost', (req, res) => {
-  //humm tem como tirar o id da thread que é pra bumpar com req.body.newPost.reply????? hummm vamo ver se da pra fazer algo pra botar esse post pra cima (baixo ja q é inverso)
-  const postReady = req.body.newPost;
-  Post.create(postReady);
-  !postReady.op ? bump(postReady.reply) : null;
-});
-
-app.post('/setNewId', (req, res) => {
-  let newIdHere = req.body.newPost.randomIdGeneratedByMe;
-  PostCount.updateOne(
-    { postNumberIs: newIdHere },
-    { $inc: { postNumberIs: +1 } },
-    (err, itemFound) => {
-      err ? console.log(err) : null;
-    }
-  );
-});
-
-app.post('/replySage', (req, res) => {
-  //sage!
-  const postReady = req.body.newPost;
-  Post.create(postReady);
-});
-
-app.post('/deletePost', (req, res) => {
-  //deleta posts que excedem a pag10, também servirá para deletar threads
-  const gonnaDelete = req.body.idDaqui;
-
-  function deleteById() {
-    Post.deleteMany(
-      { randomIdGeneratedByMe: gonnaDelete.randomIdGeneratedByMe },
-      (err) => {
-        err ? console.log(err) : null;
-      }
+app.post('/newpost', async (req, res) => {
+  try {
+    //  pega id
+    const lastId = await PostCount.findOne();
+    const { postNumberIs } = lastId;
+    //  bota id no objeto do post
+    const postReady = req.body.newPost;
+    const postToSend = {
+      ...postReady,
+      randomIdGeneratedByMe: postNumberIs,
+    };
+    //  envia
+    await Post.create(postToSend);
+    await PostCount.findOneAndUpdate(
+      { postNumberIs: postNumberIs },
+      { postNumberIs: postNumberIs + 1 }
     );
+    !postToSend.op ? bump(postToSend.reply) : null;
+    return sendStatus(200);
+  } catch (err) {
+    return sendStatus(err);
   }
-
-  function deleteByReplys() {
-    Post.deleteMany({ reply: gonnaDelete.randomIdGeneratedByMe }, (err) => {
-      err ? console.log(err) : null;
-    });
-  }
-
-  deleteById();
-  deleteByReplys();
 });
 
-app.post('/deletePostButton', (req, res) => {
-  //deleta posts que excedem a pag10, também servirá para deletar threads
+app.post('/replySage', async (req, res) => {
+  try {
+    //  pega id
+    const lastId = await PostCount.findOne();
+    const { postNumberIs } = lastId;
+    //  bota id no objeto do post
+    const postReady = req.body.newPost;
+    const postToSend = {
+      ...postReady,
+      randomIdGeneratedByMe: postNumberIs,
+    };
+    //sage!
+    await Post.create(postToSend);
+    await PostCount.findOneAndUpdate(
+      { postNumberIs: postNumberIs },
+      { postNumberIs: postNumberIs + 1 }
+    );
+    return sendStatus(200);
+  } catch (err) {
+    return sendStatus(200);
+  }
+});
 
-  const gonnaDeleteId = req.body.teste[0].threadsData._id;
-  const password = req.body.teste[1];
-  const forDeletingReplys = req.body.teste[0].threadsData.randomIdGeneratedByMe;
+//  deleta threads que excedam a pag10
+app.post('/deletePost', async (req, res) => {
+  try {
+    const gonnaDelete = req.body.idDaqui;
+    //  deleta a thread com id tal
+    await Post.deleteMany({
+      randomIdGeneratedByMe: gonnaDelete.randomIdGeneratedByMe,
+    });
+    //  deleta todas respostas dessa thread
+    await Post.deleteMany({ reply: gonnaDelete.randomIdGeneratedByMe });
+    return sendStatus(200);
+  } catch (err) {
+    return sendStatus(err);
+  }
+});
 
-  Post.find({ _id: gonnaDeleteId }, (err, findResult) => {
-    if (findResult[0].password === password) {
-      deleteById();
-      deleteByReplys();
+//  checa password antes de deletar uma thread manualmente,
+//  levando junto com suas respostas
+app.post('/deletePostButton', async (req, res) => {
+  try {
+    const threadToRemove = req.body.teste[0].threadsData._id;
+    const password = req.body.teste[1];
+    const findItsReplys = req.body.teste[0].threadsData.randomIdGeneratedByMe;
+
+    const find = await Post.find({ _id: gonnaDeleteId });
+    const getPassword = find[0].password;
+    if (getPassword === password) {
+      await Post.deleteMany({ _id: threadToRemove });
+      await Post.deleteMany({ reply: findItsReplys });
     }
-  });
-
-  function deleteById() {
-    Post.deleteMany({ _id: gonnaDeleteId }, (err) => {
-      err ? console.log(err) : null;
-    });
-  }
-  function deleteByReplys() {
-    Post.deleteMany({ reply: forDeletingReplys }, (err) => {
-      err ? console.log(err) : null;
-    });
+    return sendStatus(200);
+  } catch (err) {
+    return sendStatus(err);
   }
 });
 
-app.post('/deleteReplys', (req, res) => {
-  const gonnaDeleteId = req.body.teste[0].replyData._id;
-  const password = req.body.teste[1];
-
-  Post.find({ _id: gonnaDeleteId }, (err, findResult) => {
-    if (findResult[0].password === password) {
-      deleteById();
+//  deleta reply após as senhas coincidirem
+app.post('/deleteReplys', async (req, res) => {
+  try {
+    const gonnaDeleteId = req.body.teste[0].replyData._id;
+    const password = req.body.teste[1];
+    const find = await Post.find({ _id: gonnaDeleteId });
+    const getPassword = find[0].password;
+    if (getPassword === password) {
+      await Post.deleteMany({ _id: gonnaDeleteId });
     }
-  });
-
-  function deleteById() {
-    Post.deleteMany({ _id: gonnaDeleteId }, (err) => {
-      err ? console.log(err) : null;
-    });
+    return sendStatus(200);
+  } catch (err) {
+    return sendStatus(200);
   }
-});
-
-app.get('/getId', (req, res) => {
-  PostCount.find({}, (err, idFound) => {
-    res.json(idFound);
-  });
 });
 
 app.listen(PORT, () => {
